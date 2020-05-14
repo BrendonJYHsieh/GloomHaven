75c8832a04a46bfac5a770ce953c0f05e7e9850b
B10815058 <65119128+B10815058@users.noreply.github.com>
2020-05-14 15:25:53 +0800

    地圖

--- EndProject/GameMap.h
+++ EndProject/GameMap.h
@@ -3,19 +3,7 @@
 #include<vector>
 using namespace std;
 
-class Obstacle //障礙物物件
-{
-public:
-	Position position;
-};
-
-class Door		//門物件
-{
-public:
-	Position position;
-};
-
-class Map : public Obstacle,public Door
+class Map
 {
 public:
 	Map(){}
@@ -25,7 +13,7 @@ public:
 	/*====value====*/
 	int Width = 0;	//寬
 	int High = 0;	//高
-	vector<vector<bool>> Game_Map;	//0:牆	1:路
-	vector<vector<Door>> Door_List;		//3:門
-	vector<vector<Obstacle>> Obstacle_List;	//2:障礙物
+	vector<vector<int>> Game_Map;	//0:牆	1:路		2:障礙物		3:門
+	vector<Position> Init_Pos;
+
 };
