a4954deeff6cb6b0f489f0cdff2f52a7f7e1066d
jack6332 <58106399+jack6332@users.noreply.github.com>
2020-05-22 22:28:34 +0800

    錯誤修正

--- EndProject/Game.cpp
+++ EndProject/Game.cpp
@@ -1143,7 +1143,7 @@ bool move_Error(int x,int y,vector<Character> play_Character, vector<Ethnicity> 
 	}
 	return true;
 }
-bool move_Error_Monster(int x, int y, vector<Character> play_Character, vector<Ethnicity> Monster, Map Game_Map)
+bool move_Error_Monster(int x, int y, vector<Character> play_Character, Map Game_Map)
 {
 	if (Game_Map.Game_Map[y][x] != 1)
 	{
